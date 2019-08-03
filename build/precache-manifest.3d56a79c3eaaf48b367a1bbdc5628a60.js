b2092292fdbf9a105b50c426890a9c362da91c82
Adam Rogala <adam.rogala.ext@unit4.com>
2019-08-04 01:16:12 +0200

    create gh-page

---
+++ build/precache-manifest.3d56a79c3eaaf48b367a1bbdc5628a60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769b2743ec4fd4dca6462b381d894acd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62ce981e08ef6a1577bc",
+    "url": "/static/css/main.af52c931.chunk.css"
+  },
+  {
+    "revision": "46a407bebc94a5bb6557",
+    "url": "/static/js/2.86ed76bb.chunk.js"
+  },
+  {
+    "revision": "62ce981e08ef6a1577bc",
+    "url": "/static/js/main.c81e239b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
